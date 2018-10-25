6ec8a992c514e51d982cbbd4078580ed08155600
Beni Ben Zikry <bbenzikry@gmail.com>
2018-10-26 01:25:32 +0300

    idiomatic way of checking for woo + compliance for multisite installation w/o storefront

--- cart-tab.php
+++ cart-tab.php
@@ -19,7 +19,7 @@
 /**
  * Check if WooCommerce is active
  **/
-if ( in_array( 'woocommerce/woocommerce.php', apply_filters( 'active_plugins', get_option( 'active_plugins' ) ), true ) ) {
+if ( class_exists( 'WooCommerce' )){
 
 	/**
 	 * Localisation
